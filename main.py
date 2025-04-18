06eea8cc844d27564c8b10157f2f7736e9dc9d34
Amaad03 <amaadrajpar03@gmail.com>
2025-04-18 17:38:04 -0400

    added more evaluation metrics

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 import numpy as np
 from keras._tf_keras.keras.preprocessing import image
 from tensorflow.keras.models import load_model
+from sklearn.metrics import classification_report, f1_score, confusion_matrix
 import cv2
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #FinalModel = 93%
 #model_test.keras = 85.58%
@@ -23,6 +26,9 @@ def preprocess_image(img_path):
     img = np.stack((img,)*3, axis=-1)
     img_array = np.expand_dims(img, axis=0)
     return img_array
+
+y_true = []
+y_pred = []
 
 total_images = 0
 correct_predictions = 0
@@ -47,6 +53,10 @@ for class_name in class_names:
             actual_class_name = 'glioma'
         elif "me" in image_name:
             actual_class_name = 'meningioma'
+
+
+        y_true.append(actual_class_name)
+        y_pred.append(predicted_class_name)
         
 
         if predicted_class_name == actual_class_name:
@@ -58,3 +68,15 @@ accuracy = (correct_predictions / total_images) * 100
 print(f"Accuracy: {accuracy:.2f}%")
 print(f"Total Images: {total_images}")
 
+print("\nClassification Report:\n")
+print(classification_report(y_true, y_pred, target_names = class_names))
+
+cm = confusion_matrix(y_true, y_pred)
+
+plt.figure(figsize=(6, 5))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=class_names, yticklabels=class_names)
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix")
+plt.tight_layout()
+plt.show()
